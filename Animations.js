1c5653ff50aa40f579bee4dd1ba6fc4848cbdccb
DOTRUONGTHINH <145415786+DOTRUONGTHINH@users.noreply.github.com>
2023-09-20 16:42:10 +0700

    Update Animations.js

--- Animations.js
+++ Animations.js
@@ -29,13 +29,13 @@ function begin(){
 
     timerId=setInterval(frame, 5);
     function frame(){
-        if(x>=300 || y>=300) {
+        if(x>=1000 || y>=1000) {
             clearInterval(timerId);//stop animation
         }
         else{
-            degrees+=3;
-            x+=1;
-            y+=1;
+            degrees+=5;
+            x+=3;
+            y+=3;
             myAnimation.style.left=x+"px";
             myAnimation.style.top=y+"px";
             myAnimation.style.transform = "rotateZ("+degrees+"deg)";
